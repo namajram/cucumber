1702a568bb7de34e33895493104e901358461868
namajram <namajram@gmail.com>
2022-11-16 19:52:07 +0530

    Done

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,20 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyNamae() {
+	System.out.println("Company Name:Green Tech");
+
+}private void companyID() {
+	System.out.println("Company ID:002");
+
+}private void companyAddress() {
+	System.out.println("Company Address:Tambaram");
+
+}public static void main(String[] args) {
+	CompanyInfo com=new CompanyInfo();
+	com.companyNamae();
+	com.companyID();
+	com.companyAddress();
+}
+
+}
